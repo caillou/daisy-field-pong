d6018da545c9d995659eb21da9564cbbf485e149
Pierre Spring <pierre.spring@caillou.ch>
2024-05-01 12:27:27 +0200

    basic bouncing ball

--- pong.cpp
+++ pong.cpp
@@ -7,40 +7,77 @@ using namespace daisy;
 
 DaisyField hw;
 
-int pos_x{0};
-int pos_y{0};
+int ballPositionX{0};
+int ballPositionY{0};
+int ballDirectionX{1};
+int ballDirectionY{1};
+int ballDiameter{2};
+int acceleration{1};
 
 void UpdateDisplay()
 {
     hw.display.Fill(false);
-
-    hw.display.DrawCircle(pos_x, pos_y, 2, true);
-
-    pos_x = (pos_x + 1) % hw.display.Width();
-    pos_y = (pos_y + 1) % hw.display.Height();
-
-    hw.display.hw.display.Update();
+    hw.display.DrawCircle(ballPositionX, ballPositionY, ballDiameter, true);
+    hw.display.Update();
 }
 
+void UpdateBallPosition()
+{
+    if(ballPositionX <= ballDiameter
+       || ballPositionX >= hw.display.Width() - ballDiameter)
+    {
+        ballDirectionX = -ballDirectionX;
+        // acceleration   = acceleration + 1;
+    }
+
+    if(ballPositionY <= ballDiameter
+       || ballPositionY >= hw.display.Height() - ballDiameter)
+    {
+        ballDirectionY = -ballDirectionY;
+        // acceleration   = acceleration + 1;
+    }
+
+
+    ballPositionX = ballPositionX + ballDirectionX * acceleration;
+    ballPositionY = ballPositionY + ballDirectionY * acceleration;
+}
+
+void drawTitleScreen()
+{
+    int padding = {5};
+
+    Rectangle rect(padding,
+                   padding,
+                   hw.display.Width() - 2 * padding,
+                   hw.display.Height() - 2 * padding);
+
+    hw.display.Fill(false);
+    hw.display.WriteStringAligned(
+        "Daisy", Font_16x26, rect, Alignment::topLeft, true);
+    hw.display.WriteStringAligned(
+        "Pong", Font_16x26, rect, Alignment::bottomRight, true);
+    hw.display.Update();
+    System::Delay(1000);
+    hw.display.Fill(false);
+}
+
+void initialzieBallPosition()
+{
+    ballPositionX = hw.display.Width() / 2;
+    ballPositionY = hw.display.Height() / 2;
+}
 
 int main(void)
 {
     hw.Init();
-    Rectangle rect;
 
-    hw.display.WriteStringAligned(
-        "Daisy", Font_16x26, rect, Alignment::centered, true);
-    System::Delay(1000);
-    hw.display.WriteStringAligned(
-        "Pong", Font_16x26, rect, Alignment::centered, true);
-    System::Delay(2000);
-
-    pos_x = hw.display.Width() / 2;
-    pos_y = hw.display.Height() / 2;
+    drawTitleScreen();
+    initialzieBallPosition();
 
     for(;;)
     {
         UpdateDisplay();
-        System::Delay(300);
+        UpdateBallPosition();
+        System::Delay(16.7f);
     }
 }
